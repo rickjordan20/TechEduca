d9d8b570d5aa05560ffcd65dda44ce996a55b1e4
rickjordan20 <rickson_jordan@outlook.com>
2026-03-30 02:58:23 -0300

    corrigindo variavel

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
-  "https://SEUUSUARIO.github.io"
+  "https://rickjordan20.github.io/"
 ];
 
 app.use(cors({
