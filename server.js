4ee16dbe091ecc05ca77b01f7723b90a61d2d206
rickjordan20 <rickson_jordan@outlook.com>
2026-03-30 03:04:49 -0300

    Corrige CORS

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: allowedOrigins,
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
